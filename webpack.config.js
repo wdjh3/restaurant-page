6058703e2c8d16c684f666845dc44360c3e552bd
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-01-30 12:22:54 +0800

    Add needed loaders for CSS and HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
   mode: "development",
@@ -7,5 +8,26 @@ export default {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
